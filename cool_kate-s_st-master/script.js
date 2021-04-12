8f818b04036e80189efc15dcbc1e7bf06774e714
KateNevzgodova <you@example.com>
2021-04-12 17:25:01 +0200

    java script for random story

---
+++ cool_kate-s_st-master/script.js
@@ -0,0 +1,129 @@
+const dataStories = [{
+        name: 'moments,just moments',
+        fandom: 'exo',
+        type: 'OOC',
+        cover: 'img/fairy_tale_fantasy_foggy_forest_haunted_house_light_beam_mist-948848.jpg',
+        coverAlt: 'Story: Moments'
+    },
+    {
+        name: 'demons, Potter',
+        fandom: 'Harry Potter',
+        type: 'OOC',
+        cover: 'img/zamok-klyuch-svet-dver.jpg',
+        coverAlt: 'Story: Potter'
+    },
+    {
+        name: 'moments,just moments',
+        fandom: 'exo',
+        type: 'OOC',
+        cover: 'img/fairy_tale_fantasy_foggy_forest_haunted_house_light_beam_mist-948848.jpg',
+        coverAlt: 'Story: Moments',
+    },
+    {
+        name: 'demons, Potter',
+        fandom: 'Harry Potter',
+        type: 'OOC',
+        cover: 'img/zamok-klyuch-svet-dver.jpg',
+        coverAlt: 'Story: Potter'
+    },
+
+    {
+        name: 'moments,just moments',
+        fandom: 'exo',
+        type: 'OOC',
+        cover: 'img/fairy_tale_fantasy_foggy_forest_haunted_house_light_beam_mist-948848.jpg',
+        coverAlt: 'Story: Moments'
+    },
+    {
+        name: 'demons, Potter',
+        fandom: 'Harry Potter',
+        type: 'OOC',
+        cover: 'img/zamok-klyuch-svet-dver.jpg',
+        coverAlt: 'Story: Potter'
+    }
+]
+window.onload = function(e) {
+
+    const container = document.getElementById('main_articles');
+    if (container) {
+        for (let index = 0; index < dataStories.length; index++) {
+            const item = dataStories[index];
+            // console.log(item);
+
+            const newArticle = document.createElement("article");
+            const div = document.createElement("div")
+            div.className = 'blockImg'
+
+            const img = document.createElement("img")
+            img.src = item.cover
+            img.alt = item.coverAlt
+            div.appendChild(img)
+            const divBlockText = document.createElement('div')
+            divBlockText.className = 'blockText'
+            const divText = document.createElement('div')
+            divText.className = 'text'
+            const span = document.createElement('span')
+            const link = document.createElement('a')
+            link.className = "text"
+            link.href = '#'
+            link.innerText = item.name
+            const divTags = document.createElement('div')
+            divTags.className = "tags"
+            const span1 = document.createElement('span')
+            span1.innerText = item.fandom
+            const span2 = document.createElement('span')
+            span2.innerText = item.type
+            divTags.appendChild(span1)
+            divTags.appendChild(span2)
+            span.appendChild(link)
+            divText.appendChild(span)
+            divText.appendChild(divTags)
+            divBlockText.appendChild(divText)
+            newArticle.appendChild(div)
+            newArticle.appendChild(divBlockText)
+            container.appendChild(newArticle)
+        }
+        const search = document.getElementById('')
+    }
+
+}
+
+function onSerch(event) {
+    event.preventDefault()
+
+    const form = new FormData(event.target);
+    const search = form.get("search").toLowerCase();
+
+
+    const container = document.getElementById('main_articles');
+    const childs = container.querySelectorAll('article')
+
+    console.log(childs)
+
+    let countVisible = 0;
+    for (let index = 0; index < dataStories.length; index++) {
+        const item = dataStories[index];
+        if (!item.name.toLowerCase().includes(search)) {
+            console.log(childs[index])
+
+            childs[index].style.display = 'none'
+        } else {
+            childs[index].style.display = 'block'
+            countVisible++;
+        }
+    }
+    let notFound = container.querySelector('.not-found')
+    if (countVisible == 0) {
+        if (!notFound) {
+            notFound = document.createElement('span')
+            notFound.className = 'not-found'
+            notFound.innerText = 'There is nothing we can show you. Sorry. Try again'
+            container.appendChild(notFound)
+        }
+    } else {
+        if (notFound) {
+            container.removeChild(notFound)
+        }
+    }
+
+}
---
+++ cool_kate-s_st-master/script_random_story.js
@@ -0,0 +1,71 @@
+const dataRandomStories = {
+    name: 'Moments,just moments',
+    fandom: 'Fandom: EXO',
+    type: 'Warnings: OOC',
+    discription: 'This is not their whole story. These are moments, just moments of their life ...',
+    likes: 1300 
+}
+
+
+const drawRandomStory = (e) => {
+    const container = document.getElementById('random_story_container')
+    console.log("drawRandomStory")
+    if (container) {
+            const newSection = document.createElement('section');
+            const div = document.createElement('div');
+            div.className = 'container';
+            div.id = 'random_story_container';
+            const divTitle = document.createElement('div');
+            divTitle.className = 'title';
+            const h2 = document.createElement('h2');
+            h2.innerText = 'A New Random Story:';
+            const divStoryBody = document.createElement('div');
+            divStoryBody.className = 'story-body';
+            const link = document.createElement('a');
+            link.href='#';
+            link.className = 'link';
+            link.innerText = dataRandomStories.name;
+            const divDetail = document.createElement('div');
+            divDetail.className = 'like';
+            const imgLike = document.createElement('img');
+            imgLike.src = 'img/pulse.svg';
+            imgLike.className = 'heart_like';
+             imgLike.alt = 'like';
+            const spanLikes = document.createElement('span');
+            spanLikes.id = 'like';
+            spanLikes.innerText = dataRandomStories.likes;
+            const imgDislike = document.createElement('img');
+            imgDislike.className = 'dislike';
+            imgDislike.src = 'img/broken-heart.svg';
+            const divDescription = document.createElement('div');
+            divDescription.className = 'description';
+            const spanFandom = document.createElement('span');
+            spanFandom.innerText = dataRandomStories.fandom;
+            const spanWarnings = document.createElement('span');
+            spanWarnings.innerText = dataRandomStories.type;
+            const spanDiscription = document.createElement('span');
+            spanDiscription.innerText = 'Discription:';
+            const p = document.createElement('p');
+            p.innerText = dataRandomStories.discription;
+            divTitle.appendChild(h2);
+            
+            divTitle.appendChild(link);
+            divDescription.appendChild(spanFandom);
+            divDescription.appendChild(spanWarnings);
+            divDescription.appendChild(spanDiscription);
+            divDescription.appendChild(p);
+            divDetail.appendChild(imgLike)
+            divDetail.appendChild(spanLikes);
+            divDetail.appendChild(imgDislike);
+            div.appendChild(divTitle);
+            div.appendChild(divStoryBody);
+            
+            divStoryBody.appendChild(divDetail);
+            divStoryBody.appendChild(divDescription);
+            newSection.appendChild(div);
+            container.appendChild(newSection);
+    }
+}
+
+
+document.getElementById('random_story_container').addEventListener("load", drawRandomStory() )
